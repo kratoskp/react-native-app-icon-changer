5db0c7fa813c3bad05df1815df5324fbcdc20646
kratoskp <42991628+kratoskp@users.noreply.github.com>
2025-07-14 11:32:35 +0800

    ADD LIB

---
+++ lib/typescript/module/src/index.d.ts
@@ -0,0 +1,5 @@
+export declare function setIcon(icon: string | null): Promise<void>;
+export declare function getAllAlternativeIcons(): Promise<string[]>;
+export declare function resetIcon(): Promise<string>;
+export declare function getActiveIcon(): Promise<string>;
+//# sourceMappingURL=index.d.ts.map
